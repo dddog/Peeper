015ad65eb9843208e7af68fd309fc1a37d290e4a
dddog <azskw1101@gmail.com>
2017-05-17 17:15:08 +0900

    Volley 테스트 파일

--- app/src/main/java/com/peepers/peeper/MainActivity.java
+++ app/src/main/java/com/peepers/peeper/MainActivity.java
@@ -1,13 +1,59 @@
 package com.peepers.peeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
+    public String name = "";
+    public TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView2);
+
+        RequestQueue requestQueue;
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String URL = "https://apis.daum.net/search/web?q=신기원&apikey=69091a7314ada1e9f1025f4f80611b6c&output=json";
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i("Response", response.toString());
+                textView.setText(response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("error", error.toString());
+            }
+        });
+
+        queue.add(jsonObjReq);
+        Log.i("getJsonData end", "end");
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("msg", "button click");
+
+            }
+        });
     }
 }

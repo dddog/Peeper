a4c544b25d65fdf65f7a867bd8dd78a971a1243c
DESKTOP-ID2E5BQ\m_yg <by.mylee@gmail.com>
2017-05-20 09:08:18 +0900

    NAVER API 테스트

--- app/src/main/java/com/peepers/peeper/ListAdapter.java
+++ app/src/main/java/com/peepers/peeper/ListAdapter.java
@@ -1,8 +1,76 @@
 package com.peepers.peeper;
 
+import android.content.Context;
+import android.support.constraint.solver.ArrayLinkedVariables;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by m_yg on 2017-05-19.
  */
 
-public class ListAdapter {
+public class ListAdapter extends BaseAdapter {
+
+    ArrayList<ListModel> items;
+    LayoutInflater inflater;
+    Context context;
+
+    TextView title;
+    TextView description;
+    TextView link;
+    TextView bloggername;
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if(convertView == null){
+            viewHolder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.custom_item, null);
+            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.item_title);
+            viewHolder.tvDesc = (TextView) convertView.findViewById(R.id.item_description);
+            viewHolder.tvLink = (TextView) convertView.findViewById(R.id.item_link);
+            viewHolder.tvBlog = (TextView) convertView.findViewById(R.id.item_bloggername);
+        }
+        else{
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        ListModel listModel = items.get(position);
+        viewHolder.tvTitle.setText(listModel.getTitle());
+        viewHolder.tvDesc.setText(listModel.getDescription());
+        viewHolder.tvLink.setText(listModel.getLink());
+        viewHolder.tvBlog.setText(listModel.getBloggername());
+        return convertView;
+    }
+    class  ViewHolder{
+        TextView tvTitle;
+        TextView tvDesc;
+        TextView tvLink;
+        TextView tvBlog;
+
+    }
+
 }
--- app/src/main/java/com/peepers/peeper/ListModel.java
+++ app/src/main/java/com/peepers/peeper/ListModel.java
@@ -5,4 +5,42 @@ package com.peepers.peeper;
  */
 
 public class ListModel {
+    private String title;
+    private String link;
+    private String description;
+    private String bloggername;
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBloggername() {
+        return bloggername;
+    }
+
+    public void setBloggername(String bloggername) {
+        this.bloggername = bloggername;
+    }
 }
